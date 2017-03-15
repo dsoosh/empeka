5260a2d813d8ccdba2f5f6f247d49f592281c592
Dariusz Suszczewicz <dariusz.suszczewicz@nokia.com>
2017-03-15 20:37:57 +0100

    Update to new endpoint structure + showing vehicles positions on static google map

--- mpk.py
+++ mpk.py
@@ -1,34 +1,91 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import print_function
-import urllib2
+
+import webbrowser
+from argparse import ArgumentParser
 import json
-import sys
+
+import requests
 
 
-BASE_URL = "http://pasazer.mpk.wroc.pl/position.php"
+class Vehicle(object):
+    def __init__(self, **data):
+        self._id = data.pop('k')
+        self._x = data.pop('x')
+        self._y = data.pop('y')
+        self._line = data.pop('name')
+
+    @property
+    def x(self):
+        return self._x
+
+    @property
+    def y(self):
+        return self._y
+
+    @property
+    def line(self):
+        return self._line
+
+    @property
+    def type(self):
+        return self.__class__.__name__.lower()
+
+    def update(self, other):
+        self._x = other._x
+        self._y = other._y
 
 
-class MpkProvider(object):
-    def __init__(self, lines):
-        self.lines = lines
+class Bus(Vehicle):
+    pass
 
-    def _prepare_request_string(self, lines):
-        request_base = 'busList[bus][]={line}'
-        if isinstance(lines, list):
-            return '&'.join(request_base.format(line=line) for line in lines)
-        elif isinstance(lines, int):
-            return request_base.format(line=lines)
 
-    def provide(self):
-        response = urllib2.urlopen(BASE_URL, data=self._prepare_request_string(self.lines))
-        return response.read()
+class Tram(Vehicle):
+    pass
+
+
+def _add_vehicle_marker(map_url, vehicle):
+    map_url += '&markers=label:{line}|{x},{y}'.format(line=vehicle.line, x=vehicle.x, y=vehicle.y)
+    return map_url
+
+
+def _prepare_request_string(lines):
+    request_base = 'busList[][]={line}'
+    if isinstance(lines, list):
+        return '&'.join(request_base.format(line=line) for line in lines)
+    elif isinstance(lines, int):
+        return request_base.format(line=lines)
+
+
+def _convert(vehicles):
+    vs = []
+    for vehicle in vehicles:
+        _type = vehicle.pop('type')
+        vs.append({'bus': Bus, 'tram': Tram}.get(_type)(**vehicle))
+    return vs
+
+
+def get(lines):
+    response = requests.post("http://pasazer.mpk.wroc.pl/position.php", data=_prepare_request_string(lines),
+                             headers={'Content-Type': 'application/x-www-form-urlencoded'})
+    if response.status_code == 200:
+        return _convert(json.loads(response.content))
+    return []
+
+
+def show(lines):
+    vehicles = get(lines)
+    map_url = "http://maps.google.com/maps/api/staticmap?center=Wroclaw,Polska&zoom=12&size=1000x1000"
+    for vehicle in vehicles:
+        map_url = _add_vehicle_marker(map_url, vehicle)
+    webbrowser.open(map_url)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("Invalid arguments number. Usage:\npython mpk.py [<number>, ...]")
-        sys.exit(1)
-    lines = list(sys.argv[1].split(','))
-    mpk = MpkProvider(lines)
-    print(mpk.provide())
+    parser = ArgumentParser()
+    parser.add_argument('line', nargs='+', help='Line number')
+    parser.add_argument('--map', '-m', action='store_true', help="Show on google maps (static)")
+    args = parser.parse_args()
+    if args.map:
+        show(args.line)
